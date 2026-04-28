cf388855ae012c7b5a9424f830dd5d5bb4b95388
ANURAG <anurag.omm11@gmail.com>
2026-04-29 00:20:30 +0530

    Fix ESLint iterable issue

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,7 @@
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = [
-  ...nextVitals,
-  ...nextTs,
+export default [
+  nextVitals,
+  nextTs,
 ];
-
-export default eslintConfig;
